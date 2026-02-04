153becc833744a5313db2d0908484ec1f8bda46e
kubwimana jean nicolas <kunicolas2018@gmail.com>
2026-02-04 16:42:52 +0200

    Move relationship_app to correct location for ALX checker

---
+++ django-models/relationship_app/query_samples.py
@@ -0,0 +1,29 @@
+from relationship_app.models import Author, Book, Library, Librarian
+
+
+# Query all books by a specific author
+def books_by_author(author_name):
+    author = Author.objects.get(name=author_name)
+    books = Book.objects.filter(author=author)
+
+    print(f"Books written by {author.name}:")
+    for book in books:
+        print("-", book.title)
+
+
+# List all books in a library
+def books_in_library(library_name):
+    library = Library.objects.get(name=library_name)
+    books = library.books.all()
+
+    print(f"Books in {library.name}:")
+    for book in books:
+        print("-", book.title)
+
+
+# Retrieve the librarian for a library
+def librarian_for_library(library_name):
+    library = Library.objects.get(name=library_name)
+    librarian = Librarian.objects.get(library=library)
+
+    print(f"Librarian for {library.name}: {librarian.name}")
